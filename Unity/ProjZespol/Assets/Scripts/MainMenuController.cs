1b6ec459fee7f849266cf18414a75a7602689630
Warpek <warp3kk@gmail.com>
2025-10-14 18:32:33 +0200

    feat: Can change volume of each sound in the settings menu
    I have also added a few sample sound effects and menu music that loops until the play button is pressed.
    These changes only work in the menu.

--- Unity/ProjZespol/Assets/Scripts/MainMenuController.cs
+++ Unity/ProjZespol/Assets/Scripts/MainMenuController.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 public class MainMenuController : MonoBehaviour
 {
+    #region Varibles
     private VisualElement mainMenu;
-    [SerializeField]private AudioSource audioSource;
+    private VisualElement mainContainer;
+    private VisualElement settingsContainer;
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioSource musicSource;
     private Button startButton;
     private Button settingsButton;
     private Button quitButton;
+    private Button backButton;
+    private Slider musicSlider;
+    private Slider sfxSlider;
 
+    #endregion
+    #region Unity Methods
     private void Awake()
     {
         mainMenu = GetComponent<UIDocument>().rootVisualElement;
     }
     private void OnEnable()
     {
-        startButton = mainMenu.Q<Button>("StartButton");
-        settingsButton = mainMenu.Q<Button>("SettingsButton");
-        quitButton = mainMenu.Q<Button>("QuitButton");
+        mainContainer = mainMenu.Q<VisualElement>("Main");
+        settingsContainer = mainMenu.Q<VisualElement>("Settings");
+
+        startButton = mainContainer.Q<Button>("StartButton");
+        settingsButton = mainContainer.Q<Button>("SettingsButton");
+        quitButton = mainContainer.Q<Button>("QuitButton");
+        backButton = settingsContainer.Q<Button>("BackButton");
+        musicSlider = settingsContainer.Q<Slider>("MusicSlider");
+        sfxSlider = settingsContainer.Q<Slider>("SFXSlider");
 
         startButton.clicked += OnStartButtonClicked;
         startButton.clicked += OnClick;
@@ -24,23 +40,53 @@ public class MainMenuController : MonoBehaviour
         settingsButton.clicked += OnClick;
         quitButton.clicked += OnExitButtonClicked;
         quitButton.clicked += OnClick;
+        backButton.clicked += OnBackButtonClicked;
+        backButton.clicked += OnClick;
+
+        musicSlider.RegisterValueChangedCallback(OnMusicVolumeChanged);
+        sfxSlider.RegisterValueChangedCallback(OnSfxVolumeChanged);
     }
+    #endregion
+    #region Button And Slider Methods
     private void OnClick()
     {
-        audioSource.Play();
-        audioSource.pitch = Random.Range(0.8f, 1.2f);
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.8f, 1.2f);
     }
     private void OnStartButtonClicked()
     {
+        SceneManager.LoadScene("SampleScene");
         Debug.Log("Start");
     }
     private void OnSettingsButtonClicked()
     {
+        mainContainer.AddToClassList("hidden");
+        settingsContainer.RemoveFromClassList("hidden");
         Debug.Log("Ustawienia");
     }
+    private void OnBackButtonClicked()
+    {
+        settingsContainer.AddToClassList("hidden");
+        mainContainer.RemoveFromClassList("hidden");
+        Debug.Log("Ustawienia");
+    }
+
     private void OnExitButtonClicked()
     {
         Debug.Log("Wyjście z gry");
         Application.Quit();
     }
+
+    private void OnMusicVolumeChanged(ChangeEvent<float> evt)
+    {
+        musicSource.volume = evt.newValue;
+        Debug.Log($"Głośność muzyki: {evt.newValue}");
+    }
+
+    private void OnSfxVolumeChanged(ChangeEvent<float> evt)
+    {
+        sfxSource.volume = evt.newValue;
+        Debug.Log($"Głośność sfx: {evt.newValue}");
+    }
+    #endregion
 }
