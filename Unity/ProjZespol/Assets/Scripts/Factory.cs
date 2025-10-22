f12ec0823d3cdbe6ad61005d87e874134669de74
Warpek <warp3kk@gmail.com>
2025-10-22 23:03:59 +0200

    feat: Added a working IdleManager for resource generation
    Works with existing Factory class. Currently there is no buy/sell feature, but necessary methods are implemented. .

--- Unity/ProjZespol/Assets/Scripts/Factory.cs
+++ Unity/ProjZespol/Assets/Scripts/Factory.cs
@@ -3,13 +3,13 @@ using System;
 [Serializable]
 public class Factory
 {
-    public string name = "Fabryka1";
+    public string name;
     public double baseCost = 1;
     public double costMultiplier = 1.05;
     public double baseProduction = 1;
     public double productionMultiplier = 1.05;
     // Number of factories of the same type owned by the player
-    public int count = 0;
+    public int count = 1;
     public double currentCost => baseCost * Math.Pow(costMultiplier, count);
     public double GetProduction() => baseProduction * productionMultiplier * count;
     public double GetSellValue()
@@ -17,5 +17,10 @@ public class Factory
         if (count <= 0) return 0;
         double multiplier = costMultiplier > 0 ? costMultiplier : 1.0;
         return baseCost * Math.Pow(multiplier, count - 1) * 0.5;
+    }
+
+    public Factory(string _name)
+    {
+        name = _name;
     }
 }
