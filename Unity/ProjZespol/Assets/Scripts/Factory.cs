f3b1a01fb871a0b0cfa82a991a62d87ce1a1e79f
Warpek <warp3kk@gmail.com>
2025-10-22 22:40:57 +0200

    Added a factory type

---
+++ Unity/ProjZespol/Assets/Scripts/Factory.cs
@@ -0,0 +1,21 @@
+using System;
+
+[Serializable]
+public class Factory
+{
+    public string name = "Fabryka1";
+    public double baseCost = 1;
+    public double costMultiplier = 1.05;
+    public double baseProduction = 1;
+    public double productionMultiplier = 1.05;
+    // Number of factories of the same type owned by the player
+    public int count = 0;
+    public double currentCost => baseCost * Math.Pow(costMultiplier, count);
+    public double GetProduction() => baseProduction * productionMultiplier * count;
+    public double GetSellValue()
+    {
+        if (count <= 0) return 0;
+        double multiplier = costMultiplier > 0 ? costMultiplier : 1.0;
+        return baseCost * Math.Pow(multiplier, count - 1) * 0.5;
+    }
+}
